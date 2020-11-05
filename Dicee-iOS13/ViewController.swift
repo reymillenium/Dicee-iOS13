478eb883136862895b62353ee3f8f79353c90b23
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-05 00:38:24 -0500

    Added IB Outlets for two images  and added the change of the image and alpha parameters on 'viewDidLoad'

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Interface Builder Outlets (IB Outlets):
+    // Allows me to reference a UI element:
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    // It gest executed when the view loads (after the first show up)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewOne.alpha = 0.5
+        
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
